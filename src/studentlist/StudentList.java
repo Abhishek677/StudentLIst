c01b466c19593737c8f358c1771c4aac422b4278
HP <HP@DESKTOP-I2I36PQ>
2021-01-28 22:16:24 +0530

    Changed array to 3

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -16,8 +16,8 @@ public class StudentList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
-        Student[] students = new Student[2];
+       // changed array size to 3
+        Student[] students = new Student[3];
         
         Scanner input = new Scanner(System.in);
         
