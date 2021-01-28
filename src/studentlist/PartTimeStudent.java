280c3b2abed32924ecbe4979776b93afe0cdbd8e
HP <HP@DESKTOP-I2I36PQ>
2021-01-29 01:41:09 +0530

    Added-part-time-student

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,24 @@
+package studentlist;
+
+/**
+ *
+ * @author HP
+ */
+public class PartTimeStudent extends Student {
+    
+    private int numCourses;
+
+    public PartTimeStudent(int numCourses, String name) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+  
+}
