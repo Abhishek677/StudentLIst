02e369af85638ac206473243eac9a37b833ac369
HP <HP@10.0.0.97>
2021-02-09 02:15:44 -0500

    Baseline before Refactoring

--- CardAPick/src/CardATrick/Card.java
+++ CardAPick/src/CardATrick/Card.java
@@ -2,9 +2,57 @@
 package CardATrick;
 
 /**
- *
-    * @author Abhishek
+ * Name: Abhishek Jassal
+ * Student ID:
+ * @author Abhishek 
  */
 public class Card {
+
+   private String suit;
+   private int value;
+   
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+   public static final String [] VALUES = {"Ace", "two", "three","four","five","six","seven","eight","nine", "ten","Jack","Queen","King"};
+
+    /**
+     * This is getter method for suit
+     * @return String
+     */
+   
     
+   
+    public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+    
+    /**
+     * This setter method for suit
+     * @param suit 
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+    
+    /**
+     * This is getter method value
+     * @return int
+     */
+    public int getValue() {
+        return value;
+    }
+    
+    /**
+     * This is setter method for value
+     * @param value 
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+   
 }
--- CardAPick/src/CardATrick/CardTrick.java
+++ CardAPick/src/CardATrick/CardTrick.java
@@ -1,14 +1,68 @@
-/*
- * Your username
- * Your Student ID
- * Your Course name
- */
-package CardATrick;
 
+package CardATrick;
+import java.util.*;
 /**
  *
  * @author Abhishek
  */
 public class CardTrick {
+   
+    public static void main(String[] args)
+    {     
+        //This is scanner class to take user input
+         Scanner input = new Scanner(System.in);
+         
+         //this is random class to generate random number
+        Random random = new Random();
+        
+        Card[] magicHand = new Card[7];
+        
+                for (int i=0; i<magicHand.length; i++)
+        {
+            int value = random.nextInt(13)+1;     // this genrates number between 1 to 13
+  
+            String suit=Card.SUITS[random.nextInt(4)]; // this generates number between 0 to 3
+            
+            Card c = new Card(suit,value);
+            
+            magicHand[i]=c;
+        }
+        System.out.println("Here are the cards in the hand");
+        for (Card c : magicHand) {
+            System.out.printf("%d of %s\n", c.getValue(), c.getSuit());
+        }
+        
+        System.out.println("\n"+"Pick a suit for your card");
+        for (int i = 0; i < Card.SUITS.length; i++) {
+            System.out.println((i + 1) + ": " + Card.SUITS[i]);
+        }
+        
+        
+        System.out.print("\n"+"Enter a suit number for your card you want to Pick: ");
+        int suit = input.nextInt();
+        
+        for (int i = 0; i < Card.VALUES.length; i++) {
+            System.out.println((i + 1) + ": " + Card.VALUES[i]);
+        }
+        
+        System.out.print("Enter a value (1 to 13): ");
+        int value = input.nextInt();
+        
+        Card userGuess = new Card(Card.SUITS[suit - 1],value);
+
+        boolean match = false;
+        for (Card c : magicHand) {
+            if (c.getValue() == userGuess.getValue()
+                    && c.getSuit().equals(userGuess.getSuit())) {
+                match = true;
+                break;
+            }
+        }
     
+        String response = match ? "Card in Magic Hand": "No match";
+        
+        System.out.println(response);
+        
+         
+    }
 }
